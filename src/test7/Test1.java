b3aebe25be2b855ea7618ecefb41079c5fb5a8aa
taolinGithub <58498771+taolinGithub@users.noreply.github.com>
2019-12-17 21:22:09 +0800

    33
    33

--- src/test7/Test1.java
+++ src/test7/Test1.java
@@ -4,5 +4,7 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println(11111);
         System.out.println(2222);
+         System.out.println(3333);
+        
     }
 }
